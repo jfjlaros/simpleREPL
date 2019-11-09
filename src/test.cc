970fde90bb9a3e7946c7f993c2f67f6b889376e4
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2019-11-09 20:54:13 +0100

    Extracted parameter parsing.

--- src/test.cc
+++ src/test.cc
@@ -1,12 +1,7 @@
 #include "help.tcc"
 #include "io.tcc"
+#include "args.tcc"
 
-#include "read.tcc"
-#include "tuple.tcc"
-#include "types.tcc"
-
-
-#define PARG Tuple<const char*, const char*>
 #define param(args...) pack(args)
 #define params(args...) pack(args)
 
@@ -19,89 +14,6 @@ RWIO IO;
 template <class... Args>
 void show(Args...) {
   IO.write(__PRETTY_FUNCTION__, "\n");
-}
-
-
-/*
- * Set default values and count the number of required parameters.
- */
-inline int _setDefault(Tuple<>&, Tuple<>&, int count) {
-  return count;
-}
-
-template <class T, class... Tail>
-int _setDefault(T& argv, Tuple<PARG, Tail...>& defs, int count) {
-  _convert(&argv.head, "0"); // Not strictly needed, but nice for debugging.
-
-  return _setDefault(argv.tail, defs.tail, count + 1);
-}
-
-template <class T, class U>
-int _setDefault(T& argv, U& defs, int count) {
-  argv.head = defs.head.tail.head;
-
-  return _setDefault(argv.tail, defs.tail, count);
-}
-
-template <class T, class U>
-int setDefault(T& argv, U& defs) {
-  return _setDefault(argv, defs, 0);
-}
-
-
-/*
- * Update a required parameter value.
- */
-inline void _updateRequired(Tuple<>&, Tuple<>&, int, int, string) {}
-
-template <class T, class... Tail>
-void _updateRequired(
-    T& argv, Tuple<PARG, Tail...>& defs, int number, int count, string value) {
-  if (number == count) {
-    _convert(&argv.head, value);
-
-    return;
-  }
-
-  _updateRequired(argv.tail, defs.tail, number, count + 1, value);
-}
-
-template <class T, class U>
-void _updateRequired(T& argv, U& defs, int number, int count, string value) {
-  _updateRequired(argv.tail, defs.tail, number, count, value);
-}
-
-template <class T, class U>
-void updateRequired(T& argv, U& defs, int number, string value) {
-  _updateRequired(argv, defs, number, 0, value);
-}
-
-
-/*
- * Update an optional parameter value.
- */
-inline void updateOptional(Tuple<>&, Tuple<>&, string) {}
-
-template <class... Tail, class U>
-void updateOptional(Tuple<bool, Tail...>& argv, U& defs, string name) {
-  if (defs.head.head == name) {
-    argv.head = !argv.head;
-
-    return;
-  }
-
-  updateOptional(argv.tail, defs.tail, name);
-}
-
-template <class T, class... Tail, class U>
-void updateOptional(Tuple<T, Tail...>& argv, U& defs, string name) {
-  if (defs.head.head == name) {
-    _convert(&argv.head, IO.read());
-
-    return;
-  }
-
-  updateOptional(argv.tail, defs.tail, name);
 }
 
 
